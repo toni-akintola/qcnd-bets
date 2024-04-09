67e11eb79300a9b5b630ff3f7937cd844da3332e
toni-akintola <oakintol@nd.edu>
2024-04-09 00:11:34 -0400

    fixed assists error

--- app/bets/books/[id]/market/[book]/props/[market]/page.tsx
+++ app/bets/books/[id]/market/[book]/props/[market]/page.tsx
@@ -1,24 +1,38 @@
 import { db } from "@/lib/db";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
+import { events, Market } from "@prisma/client";
 
 export default async function Books({
   params,
 }: {
-  params: { id: string; book: string };
+  params: { id: string; book: string; market: string };
 }) {
-  const event = await db.events.findFirst({
+  const event = (await db.events.findFirst({
     where: {
       id: params.id,
     },
-  });
-  const outcomes = event?.bookmakers
+  })) as events;
+
+  const markets = event?.bookmakers
     .filter((bookmaker) => bookmaker.title == params.book)
-    .map((bookmaker) => bookmaker.markets[0].outcomes);
+    .map((bookmaker) => bookmaker.markets);
+
+  const filteredMarkets = markets[0];
+
+  const market = filteredMarkets.filter(
+    (market) => market.key === params.market,
+  );
+
+  const outcomes = market[0].outcomes;
+  console.log(outcomes);
+  // const outcomes = event?.bookmakers
+  //   .filter((bookmaker) => bookmaker.title == params.book)
+  //   .map((bookmaker) => bookmaker.markets.outcomes);
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={outcomes![0]} />
+      <DataTable columns={columns} data={outcomes} />
     </div>
   );
 }
